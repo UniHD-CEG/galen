ab33a35c72a722ef31e70a4e6d07fffdddc4fba4
Jonas Gann <jonas@gann.digital>
2023-07-18 12:59:59 +0200

    chore: test pruning of models from github

--- scripts/search_p.sh
+++ scripts/search_p.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 python -m tools.search_policy \
-  --model resnet18_cifar \
-  --ckpt_load_path results/checkpoints/cifar/resnet18_cifar.pth \
+  --model cifar10_resnet20@chenyaofo/pytorch-cifar-models \
   --log_dir ./logs \
   --agent independent-single-layer-pruning \
   --episodes 410 \
   --add_search_identifier exp_01a_prune \
-  --alg_config num_workers=6 reward=r6 mixed_reference_bits=6 reward_target_cost_ratio=0.25 tvm_target='tvm.target.arm_cpu("rasp4b")' tvm_device_key=pi-cluster-head-pi4b tvm_rpc_ip=localhost tvm_rpc_port=9000
+  --alg_config num_workers=6 reward=r6 mixed_reference_bits=6 reward_target_cost_ratio=0.25 enable_latency_eval=False reward_episode_cost_key=BOPs
